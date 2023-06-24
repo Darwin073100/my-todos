0991aa8ad4e760c8a32020119e64d19f6a27827f
Edwin Garcia Quiterio <edwinegq@outlook.com>
2023-06-24 17:35:05 -0600

    diseño inicial

--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,7 @@ module.exports ={
                 test: /\.(css|scss)$/,
                 use:[
                     "style-loader",
-                    "css-loader",
-                    "sass-loader"
+                    "css-loader"
                 ],
             },
             {
