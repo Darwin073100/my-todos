4fc64e7fb4133aa941e8a0bafa98123e9b75e533
Edwin Garcia Quiterio <edwinegq@outlook.com>
2023-06-24 19:02:22 -0600

    primeros estados

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = useState('');
+  console.log('Los usuarios buscan todo de : ' + searchValue);
   return (
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
+      value={searchValue}
+      onChange={(e)=>{
+        setSearchValue(e.target.value);
+      }}
     />
   );
 }
 
-export { TodoSearch };
+export { TodoSearch }; 
