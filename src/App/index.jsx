1402ae443e78a11aa9dfb19cffa013c7004d32cf
Edwin Garcia Quiterio <edwinegq@outlook.com>
2023-06-24 19:21:42 -0600

    contando todos completados

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -8,15 +8,25 @@ import { CreateTodoButton } from '../CreateTodoButton';
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el Curso de Intro a React.js', completed: false },
-  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'Llorar con la Llorona', completed: true },
   { text: 'LALALALALA', completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = useState(defaultTodos);
+
+  const [searchValue, setSearchValue] = useState('');
+  console.log('Los usuarios buscan todo de : ' + searchValue);
+  
+  const completedTodos = todos.filter(todo => !!todo.completed == true).length;
+  const totalTodo = todos.length;
+  
   return (
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodo} />
+      <TodoSearch 
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue} />
 
       <TodoList>
         {defaultTodos.map(todo => (
--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = useState('');
-  console.log('Los usuarios buscan todo de : ' + searchValue);
+function TodoSearch({searchValue, setSearchValue}) {
   return (
     <input
       placeholder="Cortar cebolla"
